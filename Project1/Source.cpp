278000104c5cdc70323871b0a6066456ca07cfbf
SwiftVC <christoph.swift@gmail.com>
2022-03-13 12:07:35 +0000

    Q17 changes

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -2,16 +2,21 @@
 *		Author:	CS
 *		13/03/2022
 * 
-*		Copied from Ch20 Q15
+*		Copied from Ch20 Q16
 *		The vector acts as a vector of pointers:
 *		- element's destructors called on close
 *		- the push_back operation creates a new pointer
 *		- the array-of-pointers, elem, is handled by an allocator.
-*		The changes from Ch20 Q15 are in the [] and * operators
-*		(and + operator, to support the * operator for pointer arithmetic)
+*		
+*		This allows the user to decide which objects are owned by the vector.
+*		(The final destructor won't delete the desired object when the ownership_vector goes out of scope)
+*		An object of the standard library std::vector<bool> is used to keep track of which not to delete,
+*		this resizes with the sz ownership_vector member, the number of constructed objects.
 * 
-*		Note:
-*		An iterator could've been added but pointer arithetic was opted for.
+*		An additional function is provided to return the pointer to an element who's ownership has changed:
+*		testobj* ownership::relieve(int i) { ownership[i] = false; return elem[i]; }
+* 
+*		Heap checks out.
 */
 
 #include <iostream>
@@ -26,19 +31,16 @@
 int main() {
 	try {
 		{
-		cs_vector dbl_vect;
-		dbl_vect.push_back(testobj(3));
-		dbl_vect.push_back(testobj(3));			// successful deallocation
-		cs_vector copy_test = dbl_vect;			// successful deep copy
-		cs_vector cons_test(5);					// successful deallocation
+			testobj* pointer{ nullptr };
+			{
+				ownership_vector dbl_vect;
+				dbl_vect.push_back(testobj(3));
+				dbl_vect.push_back(testobj(3));			// successful deallocation
 
-
-		// The following assignments are affected by Ch20 Q15-Q16 notational update.
-		dbl_vect[dbl_vect.size() - 1] = testobj(5);	// dbl_vect.elem[1] is changed to testobj.val = 5
-		// ^ memory assignment is fine too, the notation will be changed in the following question
-
-		*dbl_vect = testobj(90);		// dbl_vect.elem[0] is changed to testobj.val = 90
-		*(dbl_vect + 1) = testobj(100);	// dbl_vect.elem[1] is changed to testobj.val = 100
+				dbl_vect[dbl_vect.size() - 1] = testobj(5);	// dbl_vect.elem[1] is changed to testobj.val = 5
+				pointer = dbl_vect.relieve(dbl_vect.size() - 1);
+			}
+			delete pointer;
 		}
 	return 0;
 	}
--- Project1/cs_vector.h
+++ Project1/cs_vector.h
@@ -10,6 +10,7 @@ resize sets all elements to the default values.
 #include "cs_allocator.h"
 #include <iostream>
 #include <exception>
+#include <vector>
 //template <typename T, typename A = cs_allocator<T>>
 
 struct testobj {
@@ -22,29 +23,33 @@ struct testobj {
 
 };
 
-class cs_vector {
+class ownership_vector {
 private:
 	int sz;
+	std::vector<bool> ownership;
 	testobj** elem;
 	int space;
 	cs_allocator<testobj*> alloc;
 
 public:
-	cs_vector() : sz{ 0 }, elem{ nullptr }, space{ 0 } {}
-	explicit cs_vector(int s) : sz{ s }, elem{ new testobj *[s] }, space{ s }
+	ownership_vector() : sz{ 0 }, ownership(sz, true), elem{ nullptr }, space{ 0 } {}
+	explicit ownership_vector(int s) : sz{ s }, ownership(sz, true), elem{ new testobj *[s] }, space{ s }
 	{		// explicit prevents implicit conversion of int to vect in function arg
 		for (int i = 0; i < sz; i++)
 			elem[i] = new testobj(0);
 	}
-	cs_vector(const cs_vector& vect) : sz{ vect.size() }, elem{ new testobj *[sz] }, space{ sz }
+	ownership_vector(const ownership_vector& vect) : sz{ vect.size() }, elem{ new testobj *[sz] }, space{ sz }
 	{
 		for (int i = 0; i < sz; i++)
 			elem[i] = new testobj(*vect.elem[i]);
 	}
 
-	~cs_vector() {
+	~ownership_vector() {
 		for (int i = 0; i < sz; i++)
-			delete elem[i];
+		{
+			if(ownership[i])
+				delete elem[i];
+		}
 
 		alloc.deallocate(elem, space);
 	}
@@ -52,18 +57,19 @@ public:
 	int capacity() const { return space; }
 	int size() const { return sz; }
 	void push_back(testobj d);
-	cs_vector& operator=(cs_vector& vect);		// return type is convention so can chain assignment
+	ownership_vector& operator=(ownership_vector& vect);		// return type is convention so can chain assignment
 	testobj& operator[](int i) { return *elem[i]; }
 	testobj& operator*() { return *elem[0]; }
 	testobj* operator+(int i) { return elem[i]; }
 	void reserve(int newalloc);
 	//void resize(int newsize, T val = T());
 	void resize(int newsize, testobj* val = nullptr);
+	testobj* relieve(int i) { ownership[i] = false; return elem[i]; }	// gain ownership of element's pointed-to object
 
 };
 
-//template <typename T, typename A> void cs_vector<typename T, typename A>::reserve(int newalloc)
-void cs_vector::reserve(int newalloc){
+//template <typename T, typename A> void ownership_vector<typename T, typename A>::reserve(int newalloc)
+void ownership_vector::reserve(int newalloc){
 	if (newalloc <= space) return;
 	testobj** p = alloc.allocate(newalloc);
 	for (int i = 0; i < sz; i++) alloc.construct(&p[i], elem[i]);
@@ -73,16 +79,17 @@ void cs_vector::reserve(int newalloc){
 	space = newalloc;
 }
 
-//template <typename T, typename A> void cs_vector<typename T, typename A>::resize(int newsize, T val) {
-void cs_vector::resize(int newsize, testobj* val) {
+//template <typename T, typename A> void ownership_vector<typename T, typename A>::resize(int newsize, T val) {
+void ownership_vector::resize(int newsize, testobj* val) {
 	reserve(newsize);
 	for (int i = sz; i < space; i++) alloc.construct(&elem[i], val); // initialize new elements if sz<space
 	for (int i = newsize; i < sz; i++) alloc.destroy(&elem[i]);		 // resets old
 	sz = newsize;
+	ownership = std::vector<bool>(sz, true);
 }
 
-//template <typename T, typename A> void cs_vector<typename T, typename A>::push_back(T d) {
-void cs_vector::push_back(testobj d) {
+//template <typename T, typename A> void ownership_vector<typename T, typename A>::push_back(T d) {
+void ownership_vector::push_back(testobj d) {
 	testobj* ptr = new testobj(d);
 	if (space == 0)
 		reserve(8);
@@ -90,10 +97,11 @@ void cs_vector::push_back(testobj d) {
 		reserve(2 * space);
 	alloc.construct(&elem[sz], ptr);
 	++sz;
+	ownership.push_back(true);
 }
 
-//template <typename T, typename A> cs_vector<typename T, typename A>& cs_vector<T,A>::operator=(cs_vector& vect) {
-cs_vector& cs_vector::operator=(cs_vector & vect) {
+//template <typename T, typename A> ownership_vector<typename T, typename A>& ownership_vector<T,A>::operator=(ownership_vector& vect) {
+ownership_vector& ownership_vector::operator=(ownership_vector & vect) {
 	// self-assignment protection
 	if (this == &vect) return *this;
 
@@ -102,12 +110,14 @@ cs_vector& cs_vector::operator=(cs_vector & vect) {
 		for (int i = 0; i < vect.size(); i++)
 			*elem[i] = vect[i];
 		sz = vect.size();
+		ownership = std::vector<bool>(sz, true);
 		return *this;
 	}
 
 	// else, increase allocation
 	delete[] elem;
 	sz = 0;
+	ownership = std::vector<bool>(sz, true);
 	space = 0;
 	resize(vect.size());
 	for (int i = 0; i < vect.size(); i++)
